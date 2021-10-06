039599619feed1351b828347271a37566bd23390
jkulvich <jkulvichi@gmail.com>
2021-10-06 22:40:15 +0300

    added img

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ const config = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: 'index.html',
+            template: 'index.html',            
         }),
 
         // Add your plugins here
